ce217f82ec0a7c5709e14656475523eeee7fbd7e
gleisonbt <brittobaptista93@gmail.com>
2018-09-25 18:30:28 -0300

    wrapping graphql queries

--- fetch_papers.py
+++ fetch_papers.py
@@ -14,6 +14,10 @@ import feedparser
 
 from utils import Config, safe_pickle_dump
 
+import requests
+from wrapper_graphQL import *
+
+
 def encode_feedparser_dict(d):
   """ 
   helper function to get rid of feedparser bs with a deep copy. 
@@ -48,7 +52,7 @@ if __name__ == "__main__":
   # parse input arguments
   parser = argparse.ArgumentParser()
   parser.add_argument('--search-query', type=str,
-                      default='cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML',
+                      default='cat:cs.CV OR cat:cs.AI OR cat:cs.LG OR cat:cs.CL OR cat:cs.NE OR cat:stat.ML',
                       help='query used for arxiv API. See http://arxiv.org/help/api/user-manual#detailed_examples')
   parser.add_argument('--start-index', type=int, default=0, help='0 = most recent API result')
   parser.add_argument('--max-index', type=int, default=10000, help='upper bound on paper index we will fetch')
@@ -58,8 +62,8 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   # misc hardcoded variables
-  base_url = 'http://export.arxiv.org/api/query?' # base api query url
-  print('Searching arXiv for %s' % (args.search_query, ))
+  # base_url = 'http://export.arxiv.org/api/query?' # base api query url
+  # print('Searching arXiv for %s' % (args.search_query, ))
 
   # lets load the existing database to memory
   try:
@@ -77,17 +81,15 @@ if __name__ == "__main__":
   for i in range(args.start_index, args.max_index, args.results_per_iteration):
 
     print("Results %i - %i" % (i,i+args.results_per_iteration))
-    query = 'search_query=%s&sortBy=lastUpdatedDate&start=%i&max_results=%i' % (args.search_query,
-                                                         i, args.results_per_iteration)
-    with urllib.request.urlopen(base_url+query) as url:
-      response = url.read()
-    parse = feedparser.parse(response)
+    #query = 'search_query=%s&sortBy=lastUpdatedDate&start=%i&max_results=%i' % (args.search_query,
+    #                                                     i, args.results_per_iteration)
+
+    entries = find_papers(args.search_query, i, args.results_per_iteration)
+
     num_added = 0
     num_skipped = 0
-    for e in parse.entries:
 
-      j = encode_feedparser_dict(e)
-
+    for j in entries:
       # extract just the raw arxiv id and version for this paper
       rawid, version = parse_arxiv_url(j['id'])
       j['_rawid'] = rawid
@@ -102,13 +104,9 @@ if __name__ == "__main__":
       else:
         num_skipped += 1
 
+
     # print some information
     print('Added %d papers, already had %d.' % (num_added, num_skipped))
-
-    if len(parse.entries) == 0:
-      print('Received no results from arxiv. Rate limiting? Exiting. Restart later maybe.')
-      print(response)
-      break
 
     if num_added == 0 and args.break_on_no_added == 1:
       print('No new papers were added. Assuming no new papers exist. Exiting.')
@@ -121,4 +119,3 @@ if __name__ == "__main__":
   if num_added_total > 0:
     print('Saving database with %d papers to %s' % (len(db), Config.db_path))
     safe_pickle_dump(db, Config.db_path)
-
